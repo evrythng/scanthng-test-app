8e2c91e6d30b18d4b8a4589b0bd416eedef1d339
C-D-Lewis <Bonsitm@gmail.com>
2018-12-07 11:18:16 +0000

    Initial app files

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # scanthng-test-app
+
 Testing app for scanthng.js stream scanning.
---
+++ scripts/index.js
@@ -0,0 +1,76 @@
+EVT.setup({ apiUrl: 'https://api.evrythng.com' });
+
+EVT.use(EVT.Scan);
+
+const UI = {
+  buttonStartCamera: document.getElementById('button-start-camera'),
+  checkboxRedirect: document.getElementById('checkbox-redirect'),
+  anchorResult: document.getElementById('result'),
+  selectMethod: document.getElementById('select-type'),
+  inputApiKey: document.getElementById('input-api-key'),
+};
+
+let app;
+
+const getQueryParam = (key) => {
+  const params = window.location.search.substring(1)
+    .split('&')
+    .reduce((res, item) => {
+      const [key, value] = item.split('=');
+      res[key] = value;
+      return res;
+    }, {});
+
+  return params[key];
+};
+
+const createFilter = () => {
+  const map = {
+    qr: { method: '2d', type: 'qr_code' },
+    ir: { method: 'ir', type: 'image' },
+    other: {method: 'auto', type: 'auto' },
+  };
+  return map[UI.selectMethod.value];
+};
+
+const startCamera = () => {
+  if(!UI.inputApiKey.value || UI.inputApiKey.value.length !== 80) {
+    alert('Please specify an Application API Key!');
+    return;
+  }
+
+  const app = new EVT.App(UI.inputApiKey.value);
+  app.scanStream({
+    filter: createFilter(),
+    containerId: 'stream_container',
+  })
+  .then((res) => {
+    if (!res.length) {
+      UI.anchorResult.innerHTML = 'No results';
+      return;
+    }
+
+    // Raw URL
+    const url = (res.length && res[0].results.length) 
+      ? res[0].results[0].redirections[0]
+      : res[0].meta.value;
+
+    UI.anchorResult.innerHTML = url;
+    UI.anchorResult.href = url;
+    
+    if (UI.checkboxRedirect.checked) {
+      app.redirect(url);
+      return;
+    }
+  })
+  .catch(alert);
+};
+
+const main = () => {
+  // Prefil from URL query param if available
+  UI.inputApiKey.value = getQueryParam('app') || '';
+
+  UI.buttonStartCamera.addEventListener('click', startCamera);
+};
+
+main();
